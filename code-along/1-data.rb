2d541fb508d0fa9fa896986bb84904383ec97f94
Davidr1002 <david.rau@kellogg.northwestern.edu>
2022-01-25 14:52:43 +0000

    progress 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -7,13 +7,28 @@
 # Numbers
 
 # Perform simple math with numbers
+puts 5+2
+puts 5-2
+puts 5/2
+
 
 # Strings
+puts "hello, world"
+
 
 # Combine strings together
+puts "Tacos are " + "awesome"
+puts "Tacos" * 3
+puts "tacos" + 3.to_s
 
 # Variables
+x = 10
+y = 3
+puts x * y
+
 
 # Combine strings and variables
+
+
 
 # String manipulation
